b0f62bfa194b0082be44b80c694c1254a3209eef
hexh <250786313@qq.com>
2022-11-18 21:55:57 +0800

    chore: change some editor settings

--- .vim/rc.lua
+++ .vim/rc.lua
@@ -1,1 +1,10 @@
-vim.opt.mouse = "a"
+vim.cmd(
+  [[
+if match(&runtimepath, 'Defx') != -1
+  call defx#custom#option('_', {
+    \   'show_ignored_files': 0,
+    \   'ignored_files': '.*,node_modules,Dockerfile*,yarn.lock,lib,build,dist,esbuild.js,package.json,tsconfig.json,yarn.lock,LICENSE,jest.config.js',
+    \ })
+endif
+]]
+)
